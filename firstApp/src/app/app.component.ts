dae9c9a65b2158406b0cfe9eadb9264fd0f33bbe
https://github.com/github-NandiNotha <nandieh99@gmail.com>
2023-04-18 16:53:49 -0700

    changes

---
+++ firstApp/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { AboutComponent } from './about/about.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'firstApp';
+}
